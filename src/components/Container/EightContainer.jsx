74f8028f803b550b787cd0a23b849f5fe88398ff
FearCleevan <jonathan.mauring17@gmail.com>
2025-09-26 22:57:03 +0800

    updates

--- src/components/Container/EightContainer.jsx
+++ src/components/Container/EightContainer.jsx
@@ -21,23 +21,26 @@ export default function EightContainer() {
         variableWidth: false,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1024, // tablets and below
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
+                    centerMode: false,  // disable center mode
                     variableWidth: false
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 768, // mobile
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    centerMode: true
+                    centerMode: false,  // no peeking cards
+                    variableWidth: false
                 }
             }
         ]
     };
+
 
     const cards = [
         {
